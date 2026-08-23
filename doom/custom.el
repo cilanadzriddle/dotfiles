b59854a10e16e46e014e8d904e159bd915245fde
cilanadzriddle <claridaddenzil47@gmail.com>
2026-08-23 16:42:28 +0800

    Second commit for the day

--- doom/custom.el
+++ doom/custom.el
@@ -1,2 +1,18 @@
 ;;; custom.el -*- lexical-binding: t; -*-
 ;; Hand-written customization settings belong in config.el!
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((visual-line-mode) (org-enforce-todo-checkbox-dependencies . t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-drawer ((t (:family "Iosevka"))))
+ '(org-hide ((t (:family "Iosevka" :foreground "#000000"))))
+ '(org-indent ((t (:family "Iosevka" :foreground "#000000"))))
+ '(org-tag ((t (:inherit default)))))
